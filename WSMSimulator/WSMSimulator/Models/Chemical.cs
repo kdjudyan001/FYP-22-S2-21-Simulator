cdfd7d2e97db3b957593335d70af19f0d347820d
kdjudyan001 <57139664+kdjudyan001@users.noreply.github.com>
2022-07-14 21:03:06 +0800

    Removed unnecessary properties from models

--- WSMSimulator/WSMSimulator/Models/Chemical.cs
+++ WSMSimulator/WSMSimulator/Models/Chemical.cs
@@ -10,26 +10,10 @@ namespace WSMSimulator.Models
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string? ChemicalId { get; set; }
 
-
-        private string? _chemicalName;
-        [Required(AllowEmptyStrings = false)]
-        [MinLength(1)]
-        public string? ChemicalName { get { return _chemicalName; } set { _chemicalName = value is null ? value : value.Trim(); } }
-
-
-        [Range(0.0, double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
         public double MinQuantity { get; set; }
 
-        [Range(0.0, double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
         public double Quantity { get; set; }
 
-
-        private string? _measureUnit;
-        [Required(AllowEmptyStrings = false)]
-        public string? MeasureUnit { get { return _measureUnit; } set { _measureUnit = value is null ? value : value.Trim(); } }
-
-
-        public string? UsageDescription { get; set; }
 
     }
 }
--- WSMSimulator/WSMSimulator/Models/User.cs
+++ WSMSimulator/WSMSimulator/Models/User.cs
@@ -1,6 +1,4 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace WSMSimulator.Models
 {
@@ -11,42 +9,7 @@ namespace WSMSimulator.Models
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string? UserId { get; set; }
 
-
-        private string? _username;
-        [Required(AllowEmptyStrings = false)]
-        [MinLength(3, ErrorMessage = "Minimum username length is 3.")]
-        public string? Username { get { return _username; } set { _username = value is null ? null : value.Trim(); } }
-
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        [Required(AllowEmptyStrings = false)]
-        [RegularExpression(@"^(?=\P{Ll}*\p{Ll})(?=\P{Lu}*\p{Lu})(?=\P{N}*\p{N})(?=[\p{L}\p{N}]*[^\p{L}\p{N}])[\s\S]{8,}$",
-            ErrorMessage = "Password must be at least 8 letters long and contains at least one lowercase, uppercase, number, and symbol.")]
-        public string? Password { get; set; }
-
-
-        public virtual string? Type { get; set; }
-
-
-        [BsonDateTimeOptions(DateOnly = false, Kind = DateTimeKind.Utc, Representation = MongoDB.Bson.BsonType.DateTime)]
-        public DateTime? CreatedAt { get; set; }
-
-
-        private string? _fullName;
-        [Required(AllowEmptyStrings = false)]
-        public string? FullName { get { return _fullName; } set { _fullName = value is null ? value : value.Trim(); } }
-
-
-        [MinLength(1)]
-        [MaxLength(1)]
-        [RegularExpression(@"^M|F$", ErrorMessage = "Gender must be \"M\" or \"F\"")]
-        public string? Gender { get; set; }
-
-        [EmailAddress]
-        public string? Email { get; set; }
-
-        [Phone]
-        public string? Phone { get; set; }
+        public string? Type { get; set; }
 
     }
 }
